673143ee5763c31ed34d3a0b626445ded093eb07
Bhupendra <cbhupendra304@gmail.com>
2020-08-21 13:06:46 +0530

    Updates

---
+++ precache-manifest.20763453c570a76ea63b95762c02a872.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdb12dd07340961ae1beb0bfb7c6250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f79dfd85a5114db87e70",
+    "url": "/static/css/2.09c74b70.chunk.css"
+  },
+  {
+    "revision": "78f989a1f7ce2ccc6693",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f79dfd85a5114db87e70",
+    "url": "/static/js/2.1c955bb5.chunk.js"
+  },
+  {
+    "revision": "80097c1312432faada68f848f38927c4",
+    "url": "/static/js/2.1c955bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78f989a1f7ce2ccc6693",
+    "url": "/static/js/main.62eac283.chunk.js"
+  },
+  {
+    "revision": "f6ddd8efc8963c0c0ecf",
+    "url": "/static/js/runtime-main.0a80d9ba.js"
+  },
+  {
+    "revision": "58bb1ef1a006a0bfc424246c1a262928",
+    "url": "/static/media/bhup.58bb1ef1.JPG"
+  }
+]);
